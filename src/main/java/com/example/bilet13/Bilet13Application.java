e8d9708854171a0886b89ea0fcf2d109dafe36eb
Anastasiyu <asy.safiulina@mail.ru>
2022-12-05 10:54:08 +0300

    Initial commit

---
+++ src/main/java/com/example/bilet13/Bilet13Application.java
@@ -0,0 +1,13 @@
+package com.example.bilet13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bilet13Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Bilet13Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bilet13/Bilet13ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bilet13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bilet13ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
